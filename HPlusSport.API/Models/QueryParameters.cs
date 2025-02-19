9be8b5e7f5c2aca6f9911ace827f886358b22183
Alejandro Gómez <a.gomez65@uniandes.edu.co>
2025-02-19 16:22:08 -0500

    Create QueryParameters.cs

---
+++ HPlusSport.API/Models/QueryParameters.cs
@@ -0,0 +1,12 @@
+﻿namespace HPlusSport.API.Models
+{
+    public class QueryParameters
+    {
+
+        const int _maxSize = 100;
+
+        public int Page { get; set; }
+
+        public int PageSize { get; set; }
+    }
+}

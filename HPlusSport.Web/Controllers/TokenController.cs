2fd8fb49941f59d7312c598c2b064bd21c96f182
Alejandro Gómez <a.gomez65@uniandes.edu.co>
2025-02-27 10:21:49 -0500

    adding token-based authentication

---
+++ HPlusSport.Web/Controllers/TokenController.cs
@@ -0,0 +1,73 @@
+﻿using HPlusSport.Web.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using static HPlusSport.Web.Areas.Identity.Pages.Account.LoginModel;
+
+namespace HPlusSport.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly UserManager<HPlusSportWebUser> userManager;
+
+        public TokenController(UserManager<HPlusSportWebUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] InputModel model)
+        {
+            var user = await userManager.FindByNameAsync(model.Email);
+            if (user == null || !await userManager.CheckPasswordAsync(user, model.Password))
+            {
+                return Unauthorized();
+            }
+            var authClaims = new List<Claim>
+            {
+                new (JwtRegisteredClaimNames.Sub, user.UserName),
+                new (JwtRegisteredClaimNames.Email, user.Email),
+                new (JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("V€r¥ $ecret (not!) V€r¥ $ecret (not!)"));
+            var date = DateTime.UtcNow;
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(authClaims),
+                Expires = date.AddMinutes(20),
+                NotBefore = date,
+                SigningCredentials = new SigningCredentials(
+                    key, SecurityAlgorithms.HmacSha256Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token),
+                expires = token.ValidTo
+            });
+        }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [Route("products")]
+        public async Task<IActionResult> GetProducts()
+        {
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync("http://localhost:5215/api/products");
+            var data = await response.Content.ReadAsStringAsync();
+
+            return Ok(data);
+        }
+    }
+}
